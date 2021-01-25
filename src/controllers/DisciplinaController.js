049eb49b7482481ad85733e0b54bc97155139d73
jorgetbg <jorgedavin@gmail.com>
2021-01-25 14:02:54 -0300

    Versão final

--- src/controllers/DisciplinaController.js
+++ src/controllers/DisciplinaController.js
@@ -56,4 +56,159 @@ module.exports = {
 
 
 
+    async aprovadosUmDisciplina(req, res) {
+        const { idDisciplina } = req.body;
+        let alunosAprovados = await fazerlista(idDisciplina, 2);
+        return res.json({
+            sucesso: true,
+            alunos: alunosAprovados
+        });
+    },
+
+
+
+    async notafinalDisciplina(req, res) {
+        const { idDisciplina } = req.body;
+        let alunosAprovados = {};
+        console.log(idDisciplina);
+        alunosAprovados = await fazerlista(idDisciplina, 2);
+        console.log('000000000000000000');
+        console.log('alunosAprovados');
+        return res.json({
+            sucesso: true,
+            alunos: alunosAprovados
+        });
+    },
+
+
+
 };
+
+async function fazerlista (idDisciplina, operador) {
+    switch (operador) {
+        case 1: {
+            let gabaritos
+            try {
+                gabaritos = await Gabarito.find({ idDisciplina: idDisciplina });
+            } catch (error) {
+                return res.status(500).json({
+                    sucesso: false,
+                    error: error
+                });
+            }
+            let respAlunos = [];
+
+            //alunos que responderam aquele gabarito
+            try {
+                for (let i = 0; i < gabaritos.length; i++) {
+                    respAlunoGabarito = await RespAluno.find({ idGabarito: gabaritos[i]._id }).populate('idAluno');
+                    //console.log(respAlunoGabarito);
+                    respAlunos = respAlunos.concat(respAlunoGabarito);
+                }
+            } catch (error) {
+                return res.status(500).json({
+                    sucesso: false,
+                    error: error
+                });
+            }
+            //console.log(respAlunos);
+
+            let alunos = {};
+
+            //for que percorre o vetor respAlunos e armazena de forma individual as respostas de um unico aluno, e joga ele em um vetor de alunos
+            respAlunos.forEach(respAluno => {
+                if (alunos[respAluno.idAluno._id] == undefined) {
+                    alunos[respAluno.idAluno._id] = {
+                        nota: null,
+                        resolucoes: [],
+                        nome: respAluno.idAluno.nome
+                    };
+                }
+                alunos[respAluno.idAluno._id].resolucoes.push(respAluno);
+
+            })
+            let alunosAprovados = [];
+            //for para opter a nota final do aluno, somando a nota de todas as provas feitas pelo aluno.
+            for (let idAluno in alunos) {
+                let nota = 0;
+                alunos[idAluno].resolucoes.forEach(resolucao => {
+                    nota = nota + resolucao.nota;
+
+                })
+                alunos[idAluno].nota = parseFloat((nota / gabaritos.length).toFixed(2));
+                if (alunos[idAluno].nota > media) {
+                    alunosAprovados.push({
+                        nome: alunos[idAluno].nome,
+                        nota: alunos[idAluno].nota
+                    });
+                }
+            }
+            return alunosAprovados;
+
+
+        }
+        case 2: {
+            console.log(idDisciplina);
+            let gabaritos
+            try {
+                gabaritos = await Gabarito.find({ idDisciplina: idDisciplina });
+            } catch (error) {
+                return res.status(500).json({
+                    sucesso: false,
+                    error: error
+                });
+            }
+            console.log(gabaritos);
+            let respAlunos = [];
+
+            //alunos que responderam aquele gabarito
+            try {
+                for (let i = 0; i < gabaritos.length; i++) {
+                    respAlunoGabarito = await RespAluno.find({ idGabarito: gabaritos[i]._id }).populate('idAluno');
+                    //console.log(respAlunoGabarito);
+                    respAlunos = respAlunos.concat(respAlunoGabarito);
+                }
+            } catch (error) {
+                return res.status(500).json({
+                    sucesso: false,
+                    error: error
+                });
+            }
+            let alunos = {};
+
+            //for que percorre o vetor respAlunos e armazena de forma individual as respostas de um unico aluno, e joga ele em um vetor de alunos
+            respAlunos.forEach(respAluno => {
+                if (alunos[respAluno.idAluno._id] == undefined) {
+                    alunos[respAluno.idAluno._id] = {
+                        nota: null,
+                        resolucoes: [],
+                        nome: respAluno.idAluno.nome
+                    };
+                }
+                alunos[respAluno.idAluno._id].resolucoes.push(respAluno);
+            })
+            console.log(respAlunos);
+            let alunosAprovados = [];
+            //for para opter a nota final do aluno, somando a nota de todas as provas feitas pelo aluno.
+            for (let idAluno in alunos) {
+                let nota = 0;
+                alunos[idAluno].resolucoes.forEach(resolucao => {
+                    nota = nota + resolucao.nota;
+                })
+                alunos[idAluno].nota = (nota / gabaritos.length).toFixed(2);
+                alunosAprovados.push({
+                    nome: alunos[idAluno].nome,
+                    nota: alunos[idAluno].nota
+                });
+            }
+            console.log('____________');
+            console.log(alunosAprovados);
+
+            return alunosAprovados;
+
+
+        }
+    }
+}
+
+
