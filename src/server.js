83c63c1d615904890cb181c0250c5476fc304f11
Jorge Davi Navarro <jorgetbg@hotmail.com>
2021-01-25 14:35:10 -0300

    Update server.js
    retirando link do banco

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const routes = require('./routes');
 const app = express();
 
-mongoose.connect('mongodb://projeto:projeto@projeto-shard-00-00.mxplu.mongodb.net:27017,projeto-shard-00-01.mxplu.mongodb.net:27017,projeto-shard-00-02.mxplu.mongodb.net:27017/projeto2?ssl=true&replicaSet=atlas-ukw6vh-shard-0&authSource=admin&retryWrites=true&w=majority', {
+mongoose.connect('insira seu link com o banco aqui', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 
